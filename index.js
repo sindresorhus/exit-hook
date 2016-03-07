3afe4698bb29633d882ea5862c8c51a04b289325
Tapppi <moilanen.tapani@gmail.com>
2016-03-07 22:33:55 +0200

    Add support for pm2 clustering

--- index.js
+++ index.js
@@ -26,5 +26,12 @@ module.exports = function (cb) {
 		process.once('exit', exit);
 		process.once('SIGINT', exit.bind(null, true, 2));
 		process.once('SIGTERM', exit.bind(null, true, 15));
+
+		// PM2 Cluster shutdown message
+		process.on('message', function(msg) {
+			if (msg === 'shutdown') {
+				exit(true, -128);
+			}
+		});
 	}
 };
