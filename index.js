0113ba6c73330d07c4aed228f1337de1e9770a2d
Tapppi <moilanen.tapani@gmail.com>
2016-03-13 01:42:21 +0700

    Close #6 PR: Add support for pm2 clustering.

--- index.js
+++ index.js
@@ -26,5 +26,14 @@ module.exports = function (cb) {
 		process.once('exit', exit);
 		process.once('SIGINT', exit.bind(null, true, 2));
 		process.once('SIGTERM', exit.bind(null, true, 15));
+
+		// PM2 Cluster shutdown message. Caught to support async handlers with pm2, needed because
+		// explicitly calling process.exit() doesn't trigger the beforeExit event, and the exit
+		// event cannot support async handlers, since the event loop is never called after it.
+		process.on('message', function (msg) {
+			if (msg === 'shutdown') {
+				exit(true, -128);
+			}
+		});
 	}
 };
