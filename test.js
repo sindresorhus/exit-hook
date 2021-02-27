27afecb37d6e460257f7254e6a8820b457aea254
Julian Waller <git@julusian.co.uk>
2021-02-27 12:53:44 +0000

    Ensure listeners are registered only once (#16)

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 import test from 'ava';
 import execa from 'execa';
+import exitHook from '.';
 
 test('main', async t => {
 	const {stdout} = await execa(process.execPath, ['fixture.js']);
 	t.is(stdout, 'foo\nbar');
+});
+
+test('listener count', t => {
+	t.is(process.listenerCount('exit'), 0);
+
+	const unsubscribe1 = exitHook(() => {});
+	const unsubscribe2 = exitHook(() => {});
+	t.is(process.listenerCount('exit'), 1);
+
+	// Remove all listeners
+	unsubscribe1();
+	unsubscribe2();
+	t.is(process.listenerCount('exit'), 1);
+
+	// Re-add listener
+	const unsubscribe3 = exitHook(() => {});
+	t.is(process.listenerCount('exit'), 1);
+
+	// Remove again
+	unsubscribe3();
+	t.is(process.listenerCount('exit'), 1);
 });
